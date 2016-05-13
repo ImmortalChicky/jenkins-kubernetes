84d2bcc26fb63dfdb8a183cca59d619fd5e733a4
Your Name <you@example.com>
2016-05-13 20:28:51 +0000

    Fix Permission error

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 gcloud compute disks create --size 20GB kubernetes-jenkins
 ssh-keygen -t rsa -b 4096 -C "jenkins" -f jenkins_ssh -q -N ""
-kubectl create secret generic jenkins-ssh --from-file=jenkins --from-file=jenkins.pub
+kubectl create secret generic jenkins-ssh --from-file="jenkins=jenkins_ssh" --from-file="jenkins.pub=jenkins_ssh.pub"
 rm jenkins_ssh*
 kubectl create -f jenkins-deployment.yaml
 kubectl create -f jenkins-service.yaml
+
