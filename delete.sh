49a2dd49f85d58093374f81d9e09f78fd470dedd
Valentin Funk <funk.valentin@gmail.com>
2016-05-13 14:20:49 +0200

    Fixes

--- delete.sh
+++ delete.sh
@@ -1,3 +1,3 @@
 kubectl delete -f jenkins-deployment.yaml
 kubectl delete -f jenkins-service.yaml
-kubectl delete secret jenkins
+kubectl delete secret jenkins-ssh
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 gcloud compute disks create --size 20GB kubernetes-jenkins
-ssh-keygen -t rsa -b 4096 -C "jenkins" -f jenkins -q -N ""
+ssh-keygen -t rsa -b 4096 -C "jenkins" -f jenkins_ssh -q -N ""
 kubectl create secret generic jenkins-ssh --from-file=jenkins --from-file=jenkins.pub
-rm jenkins*
+rm jenkins_ssh*
 kubectl create -f jenkins-deployment.yaml
 kubectl create -f jenkins-service.yaml
